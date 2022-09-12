760988075a27d0bfce926f7eaf5318eea5ffb835
Maytham Ghaly <maythamghaly@gmail.com>
2022-09-12 09:54:04 +0300

    delet the join and use where only

--- Exercice 1/Quere.sql
+++ Exercice 1/Quere.sql
@@ -1,4 +1,3 @@
 select min( movie.earnings_rank)  , movie.name
-from movie 
-INNER JOIN oscar ON movie.idMovie=oscar.Movie_idMovie
+from movie ,oscar
 WHERE oscar.Type="best_picture" and movie.earnings_rank=(SELECT min(movie.earnings_rank) FROM movie)
